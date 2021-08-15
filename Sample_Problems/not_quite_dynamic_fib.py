cf3dd80cfe78460c810c80a697704b02cfa24086
Aldridge <sherialdridge@gmail.com>
2021-08-15 13:44:03 -0400

    Update not_quite_dynamic_fib.py

--- Sample_Problems/not_quite_dynamic_fib.py
+++ Sample_Problems/not_quite_dynamic_fib.py
@@ -2,15 +2,22 @@
 I don't think this would be considered dynamic programming because there's no recursion,
 but it's my preferred solution. The time complexity should still = O(n), and I don't have 
 to write any conditionals for n = 0 or n = 1. 
+
+I haven't included any checks here. It's a toy problem. There's no need to complicate the code. 
 '''
 
+fn = [0, 1]
+
 def fib(n):
-    fn = [0, 1]
     m = len(fn)
-    while m <= n:
-        f = fn[-2] + fn[-1]
-        fn.append(f)
-        m+=1
+    # If the param is < the length of fn, then fib(n) will already be defined. 
+    if n <= m:
+        f = fn[n]
+    else: 
+        while m <= n:
+            f = fn[-2] + fn[-1]
+            fn.append(f)
+            m+=1
     return f"Fib of {n} is {f}"
 
 print(fib(8))
