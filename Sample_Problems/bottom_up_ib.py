9ec31658f2012fe95a0293fd05b77116a09e2e8a
Aldridge <sherialdridge@gmail.com>
2021-08-15 15:07:43 -0400

    Update and rename not_quite_dynamic_fib.py to bottom_up_ib.py

---
+++ Sample_Problems/bottom_up_ib.py
@@ -0,0 +1,13 @@
+fn = [0, 1]
+
+def fib(n):
+    m = len(fn)
+    if n < m:
+        f = fn[n]
+    else: 
+        for m in range(m, n):
+            f = fn[-2] + fn[-1]
+            fn.append(f)
+    return f"Fib of {n} is {f}"
+
+print(fib(8))
