f3439c2cb4223eb2c68e0ed0810799d7c72d9413
Aldridge <sherialdridge@gmail.com>
2021-08-15 13:38:59 -0400

    Update not_quite_dynamic_fib.py

--- Sample_Problems/not_quite_dynamic_fib.py
+++ Sample_Problems/not_quite_dynamic_fib.py
@@ -2,9 +2,6 @@
 I don't think this would be considered dynamic programming because there's no recursion,
 but it's my preferred solution. The time complexity should still = O(n), and I don't have 
 to write any conditionals for n = 0 or n = 1. 
-Because this is bottom-up and incremental, I don't need recursion. I know that any previous
-soluitons are already in my list. If I were defining fn outside of the function, I'd probably 
-use a dictionary instead because this function relies on the list being ordered.
 '''
 
 def fib(n):
