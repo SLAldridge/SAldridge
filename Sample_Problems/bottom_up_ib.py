8103bea013fde56413d9d71166ab7a423619414b
Aldridge <sherialdridge@gmail.com>
2021-08-15 16:00:13 -0400

    Update bottom_up_ib.py

--- Sample_Problems/bottom_up_ib.py
+++ Sample_Problems/bottom_up_ib.py
@@ -11,3 +11,25 @@ def fib(n):
     return f"Fib of {n} is {f}"
 
 print(fib(8))
+print(fib(0))
+print(fib(1))
+print(fib(2))
+
+#alternatively 
+def fib2(n):
+    fn2 = [0, 1]
+    if n in [0, 1]: f = n
+    else: 
+        i = len(fn2)
+        while i <= n:
+            f = fn2[-2] + fn2[-1]
+            fn2.pop(0)
+            fn2.append(f)
+            i+=1
+    print(fn2)
+    return f"Fib of {n} is {f}"
+
+print(fib2(8))
+print(fib2(0))
+print(fib2(2))
+print(fib2(1))
